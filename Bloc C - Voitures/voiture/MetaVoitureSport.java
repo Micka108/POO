a54dd22cc095d08caaf63ef3c6264835569379f0
Micka108 <34743212+Micka108@users.noreply.github.com>
2020-03-30 15:48:01 -0400

    Delete MetaVoitureSport.java

--- Bloc C - Voitures/voiture/MetaVoitureSport.java
+++
@@ -1,9 +0,0 @@
-package voiture;
-
-public class MetaVoitureSport extends VoitureSport implements Surveillable {
-
-    @Override
-    public int surveiller(int limite){
-        return limite - vitesse;
-    }
-}
